0a909705d69999521d97187bde79c2fcc4b2ae4e
Abdullahalmuhit <abdullahalmuhit1296@gmail.com>
2022-03-15 10:35:15 +0600

    Added frontend

---
+++ ecommerce_dashboard/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MasterLayout from './layouts/admin/MasterLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <MasterLayout />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce_dashboard/src/layouts/admin/MasterLayout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const MasterLayout = () => {
+    return (
+        <div className='sb-nav-fixed'>
+            <Navbar />
+        </div>
+
+    );
+    
+
+}
+export default MasterLayout;
